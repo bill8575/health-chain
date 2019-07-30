a457defbf85f33848b5c21adf4068e1bac68ecee
Bill Leung <bill8575@gmail.com>
2019-07-30 11:00:27 +0800

    adding submitMedRec, verifyMedRec, etc

--- contracts/HealthChain.sol
+++ contracts/HealthChain.sol
@@ -8,6 +8,8 @@ contract HealthChain {
 
     mapping (uint => bool) public docIdExists;
 
+    mapping (address => uint) docIdLookup;
+
     struct doctor {
         address doctor;
         uint doctorId;
@@ -21,26 +23,35 @@ contract HealthChain {
       uint mrn;
       uint doctorId;
     }
-    
-    // uint public doctorCount;
-    uint public clncRegCount;
+
     mapping (uint => patientRegister) public clinicRegistry;
+
+    struct medRecord {
+        bool verified;
+        uint doctorId;
+        uint mrn;
+        bytes data;
+    }
+
+    mapping (uint => medRecord) public patientMedRecords;
+
+    uint public medRecCount;
+    uint public clncRegCount;
 
     modifier mustBeAnAdmin() { require (isAdmin[msg.sender]); _;}
     
     modifier notAlreadyDoctor() { require (!isDoctor(msg.sender)); _;}
 
+    modifier isADoctor() { require (isDoctor(msg.sender)); _;}
+
     modifier doctorExists(uint _doctorId) { require (docIdExists[_doctorId]); _;}
-
-    // modifier requiring that patient address cannot equal to 
-    // a doctor's with which he/she is registering
-
 
     constructor(address[] memory _starterDoctors, 
             address[] memory _admins) 
             public 
     {
         clncRegCount = 0;
+        medRecCount = 0;
 
         // To have a couple of doctors created to start
         // 
@@ -54,6 +65,8 @@ contract HealthChain {
             docIdExists[inputDocId] = true;
         }
 
+        // Pre-defined which accounts are the admins 
+        //   through the deployment script
         for (uint i=0; i<_admins.length; i++) {
             isAdmin[_admins[i]] = true;
         }
@@ -64,6 +77,9 @@ contract HealthChain {
         revert();
     }    
 
+    // Only the admins are allowed to delete a doctor 
+    //   parameters are matching doctorId and doctor's 
+    //   address
     function deleteDoctor(uint _doctorId, address _doctor) 
         public 
         mustBeAnAdmin()
@@ -82,6 +98,7 @@ contract HealthChain {
         }
     }
     
+    // Anyone can enrol as a doctor 
     function createDoctor(uint _doctorId) 
         public
         notAlreadyDoctor()
@@ -90,17 +107,23 @@ contract HealthChain {
         doctor memory newDoctor = doctor(msg.sender, _doctorId);
         doctors.push(newDoctor);
 
+        docIdLookup[msg.sender] = _doctorId;
+
         return doctors.length;
     }
     
+    // function to return the total 
+    //   number of doctors enrolled
+    //   at the moment 
     function getTotalDoctors() 
         public view
         returns (uint) 
     {
         return doctors.length;
-        // return doctorCount;
     }
 
+    // function to return the entire doctors
+    //   array
     function getDoctors()
         public view
         // returns (doctor memory)         
@@ -111,6 +134,8 @@ contract HealthChain {
         return doctors;
     }
 
+    // function to test if a contract caller is 
+    //   enrolled as a doctor
     function isDoctor(address _doctor) 
         public view
         returns (bool)    
@@ -151,6 +176,9 @@ contract HealthChain {
         return _doctorId;
     }
     
+    // function to test if the contract caller is 
+    //   a registered patient of the doctor
+    //   with the provided _doctorId
     function isPatientOf(uint _doctorId) 
         public view
         doctorExists(_doctorId)
@@ -166,12 +194,74 @@ contract HealthChain {
 
         return (foundRegRec);        
     }
-    
-    function submitMedRec(address patient, bytes memory data) 
+
+    // User must be a doctor to look up its own
+    //   patient's MRN. There can be only one registry
+    //   record for a given patient for the doctor
+    function getPatientMRN(address _patient) 
         public 
+        view
+        isADoctor()
+        returns (uint _mrn)
+    {
+        uint doctorId = docIdLookup[msg.sender];
+
+        uint totalRegRec = clncRegCount;
+        for (uint i=0; i<totalRegRec; i++) {
+            if ((clinicRegistry[i].patient == _patient) 
+               && (clinicRegistry[i].doctorId == doctorId))
+                _mrn = clinicRegistry[i].mrn;
+        }        
+
+        return _mrn;
+
+    }
+    
+    // Only doctor can submit a medical record
+    // Doctor can only submit medRec for its enrolled patient
+    function submitMedRec(address _patient, bytes memory _data) 
+        public 
+        isADoctor()
         returns (uint medRecId) 
     {
-           
+        uint patientMRN = getPatientMRN(_patient);
+
+        patientMedRecords[medRecCount] = medRecord({
+            verified: false,
+            doctorId: docIdLookup[msg.sender],
+            mrn: patientMRN,
+            data: _data
+        });
+        medRecId = medRecCount;
+        medRecCount += 1;
+
+    }
+
+    // The confirmer has to be the patient for
+    // which is treated by the doctor with the doctorId
+    // stored in the medicalRecord
+    //
+    // Loop through the clinicRegistry and search for 
+    //   matching criteria based on mrn looked up from 
+    //   patientMedRecords with the parameter medRecId
+    function verifyMedRec(uint medRecId) 
+        public
+        returns (bool IsVerified)
+    {
+        IsVerified = false;
+        medRecord storage m = patientMedRecords[medRecId];
+        uint totalRegRec = clncRegCount;
+        for (uint i=0; i<totalRegRec; i++) {
+            if ((clinicRegistry[i].patient == msg.sender) 
+                && (clinicRegistry[i].mrn == m.mrn)
+                && isPatientOf(clinicRegistry[i].doctorId)) {
+                m.verified = true;
+                IsVerified = true;
+
+            }
+                
+        }        
+
     }
     
 }
